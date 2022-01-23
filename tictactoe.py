cc34ba25819476ef85bc20ddea4a9ba5fefbdf82
rumacalin <russell.macalintal@gmail.com>
2022-01-23 16:30:38 -0500

    Added reference display for game board showing index 1-9

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,28 @@
-print('Hello World')
+# FUNCTION FOR DISPLAY BOARD STATE
+def display_board(board):
+    print(f"| {board[0]} | {board[1]} | {board[2]} |       | 1 | 2 | 3 |")
+    print("-------------       -------------")
+    print(f"| {board[3]} | {board[4]} | {board[5]} |       | 4 | 5 | 6 |")
+    print("-------------       -------------")
+    print(f"| {board[6]} | {board[7]} | {board[8]} |       | 7 | 8 | 9 |")
+
+def player_input(player):
+    if player == "X":
+        print("Player 1's (X) Turn")
+    elif player == "O":
+        print("Player 2's (O) Turn")
+
+    i = input("Enter ")
+
+
+# FUNCTION TO INITILIAZE AND PLAY GAME
+def play_game():
+    # DEFINE INITIAL BOARD WITH BLANK SPACES AND SET PLAYER 1 AS 'X' AND PLAYER 2 AS 'O'
+    game_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    pl_1 = "X"
+    pl_2 = "O"
+
+    display_board(game_board)
+
+# START GAME
+play_game()
