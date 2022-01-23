2e95a56a3cb69080a7d735e3419173bcb1f5cfd0
rumacalin <russell.macalintal@gmail.com>
2022-01-23 17:20:04 -0500

    Added player input functions and game end/winning conditions

--- tictactoe.py
+++ tictactoe.py
@@ -6,23 +6,41 @@ def display_board(board):
     print("-------------       -------------")
     print(f"| {board[6]} | {board[7]} | {board[8]} |       | 7 | 8 | 9 |")
 
+
 def player_input(player):
+    # INITIAL INPUT VARIABLE FOR WHILE LOOP CONDITIONAL
+    i = "START"
+    acc_range = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     if player == "X":
         print("Player 1's (X) Turn")
     elif player == "O":
         print("Player 2's (O) Turn")
 
-    i = input("Enter ")
+    while i.isdigit() == False or i not in acc_range:
+        i = input("Select location [1-9]: ")
+
+    return int(i)
+
+
+def update_board(board, player_input):
+
 
 
 # FUNCTION TO INITILIAZE AND PLAY GAME
 def play_game():
-    # DEFINE INITIAL BOARD WITH BLANK SPACES AND SET PLAYER 1 AS 'X' AND PLAYER 2 AS 'O'
+    # DEFINE EIGHT POSSIBLE WINNING CONDITIONS
+    win_list = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    # DEFINE INITIAL BOARD WITH BLANK SPACES AND SET FIRST PLAYER AS 'X'
     game_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    pl_1 = "X"
-    pl_2 = "O"
+    empty_space = " "
+    pl = "X"
 
-    display_board(game_board)
+    # SET WHILE LOOP TO KEEP PLAYING WHILE THERE'S AN EMPTY SPACE ON THE BOARD OR UNTIL A WINNING CONDITION IS MET
+    while empty_space in game_board:
+        display_board(game_board)
+        player_input(pl)
+
+
 
 # START GAME
 play_game()
